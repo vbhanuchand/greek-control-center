a09fa9e9800c0c322ae5de53af866b7aeebfb20d
v.bhanuchand <v.bhanuchand@gmail.com>
2013-10-30 10:32:23 +0000

    Labor Calendar Changes

--- control/src/com/services/core/view/utils/Test.java
+++ control/src/com/services/core/view/utils/Test.java
@@ -6,8 +6,10 @@ public class Test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Dates Testing --> " + Utilities.getStringforLaborSkeleton("2013-10-26") + " New --> " + Utilities.newGetStringforLaborSkeleton("2013-10-26"));
-		System.out.println("Dates Testing --> " + Utilities.getWeekNumber("2013-10-27") + " New --> " + Utilities.newGetWeekNumber("2013-10-27"));
+		//System.out.println("Dates Testing --> " + Utilities.getStringforLaborSkeleton("2013-09-14") + " New --> " + Utilities.newGetStringforLaborSkeleton("2013-09-14"));
+		//System.out.println("Dates Testing --> " + Utilities.getWeekNumber("2013-10-27") + " New --> " + Utilities.newGetWeekNumber("2013-10-27"));
+		
+		System.out.println("Dates Testing --> " + Utilities.newGetStartDateEndDate("2013-42"));
 	}
 
 }
